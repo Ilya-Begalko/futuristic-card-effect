53462aea99c66429f6c62e84670c482e24382fd7
Ilya Begalko <ilja.begalko@yandex.ru>
2023-04-12 21:12:23 +0600

    add html and scripts

---
+++ script.js
@@ -0,0 +1,31 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+const screen = document.querySelector(".screen"),
+    name = document.querySelector(".name");
+
+screen.onmouseenter = event => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        name.innerText = name.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return name.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+
+        if (iteration >= name.dataset.value.length) {
+            clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+    }, 30);
+}
